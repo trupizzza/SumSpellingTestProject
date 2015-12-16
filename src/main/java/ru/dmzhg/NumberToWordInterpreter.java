3ea7bed2867abcb3d991597ead22c81a90ae60c9
trupizzza <trupizzza@gmail.com>
2015-12-16 18:56:51 +0300

    Make simplified tests.

--- src/main/java/ru/dmzhg/NumberToWordInterpreter.java
+++ src/main/java/ru/dmzhg/NumberToWordInterpreter.java
@@ -1,51 +1,32 @@
 package main.java.ru.dmzhg;
 
-import java.math.BigDecimal;
-
 /**
  * Created by DMZHG on 11.12.2015.
  */
 public class NumberToWordInterpreter {
-    public static String Spell(BigDecimal numberToBeSpelled) {
-        String spelledNumber = "";
-        String numberGroups[] = new String[10];
-        int groupCount = 0;
-        BigDecimal bd = new BigDecimal(555.833);
-        int bdi = bd.intValue();
-        String stringoBeSpelled = numberToBeSpelled.toString();
-        //
-        String cleanNumberString = stringoBeSpelled.replace(",", ".").replace(" ", "");
-        // Detecting amount of number triads in number
-        if (cleanNumberString.length() % 3 == 0) {
-            groupCount = cleanNumberString.length() / 3;
-        } else {○
-            groupCount = 1 + cleanNumberString.length() / 3;
-        }
-        if (cleanNumberString.length() <= 3) {
-            return spellGroup();
-        }
-        for (int i = 0; i <= cleanNumberString.length() / 3; i++) {
-            if (cleanNumberString.length() <= 3) {
-                numberGroups[i + 1] = cleanNumberString;
-            }
-            numberGroups[i] = stringoBeSpelled.substring(cleanNumberString.length() - 3, cleanNumberString.length());
-            cleanNumberString = cleanNumberString.substring(cleanNumberString.length() * i);
+    public static String translate(final int number) {
+        String resultingWord = null;
+        switch (number) {
+            case 1:
+                resultingWord = "one";
+                break;
+            case 5:
+                resultingWord = "five";
+                break;
+            case 10:
+                resultingWord = "ten";
+                break;
+            case 14:
+                resultingWord = "fourteen";
+                break;
+            default:
+                resultingWord = "Translation error!";
 
         }
-        for (int i = 0; cleanNumberString.length() < 3; i++) {
-            System.out.println(numberGroups[i]);
-        }
-
-        return spelledNumber;
-
+        return resultingWord;
     }
 
-    private static String spellGroup() {
-        return "";
-    }
-
-    private static void hapinness() {
-        System.out.println("I'm so happy to make my very virst commit to Git!9");
-    }
 }
+
+
 
--- src/test/java/ru/dmzhg/SumSpellingTest.java
+++ src/test/java/ru/dmzhg/SumSpellingTest.java
@@ -1,7 +1,6 @@
 package test.java.ru.dmzhg;
 
 import main.java.ru.dmzhg.NumberToWordInterpreter;
-import main.java.ru.dmzhg.SummaSpellingOut;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,13 +15,22 @@ import static org.junit.Assert.assertEquals;
 public class SumSpellingTest {
     private Integer sum;
     private Integer numberToBeSpelled;
+    private Integer engNum;
+    private Integer fiveNumber;
+    private int oneNumber;
+    private int tenNumber;
+    private int fourteenNumber;
+    private int thirtyFiveNumber;
 
     @Before
     public void setUp() {
 
+        fiveNumber = 5;
+        oneNumber = 1;
+        tenNumber = 10;
+        fourteenNumber = 14;
+        thirtyFiveNumber = 35;
 
-        numberToBeSpelled = new Integer(1150453);
-        sum = new Integer(500);
     }
 
     @After
@@ -31,20 +39,27 @@ public class SumSpellingTest {
     }
 
     @Test
-    public void shouldTranslateIntegerToWord() {
-
-        String numbers = SummaSpellingOut.getAmountWithWord(sum.toString());
-        int indexQty = numbers.indexOf("руб");
-        numbers = numbers.substring(0, indexQty - 1);
-        assertEquals("Пятьсот", numbers);
-
+    public void shouldTranslateNumberFiveToWord() {
+        assertEquals("five", NumberToWordInterpreter.translate(fiveNumber));
     }
 
     @Test
-    public void shouldTranslateMillionToWord() {
-        assertEquals("Один миллион сто пятьдесят тысяч четыреста пятьдесят три руб. 00 коп.", NumberToWordInterpreter.Spell(numberToBeSpelled));
+    public void shouldTranslateNumberOneToWord() {
+        assertEquals("one", NumberToWordInterpreter.translate(oneNumber));
+    }
 
+    @Test
+    public void shouldTranslateNumberTenToWord() {
+        assertEquals("ten", NumberToWordInterpreter.translate(tenNumber));
+    }
 
-//    }
+    @Test
+    public void shouldTranslateNumberFourteenToWord() {
+        assertEquals("fourteen", NumberToWordInterpreter.translate(fourteenNumber));
+    }
+
+    @Test
+    public void shouldTranslateThirtyFiveNumberToWord() {
+        assertEquals("thirty five", NumberToWordInterpreter.translate(thirtyFiveNumber));
     }
 }
